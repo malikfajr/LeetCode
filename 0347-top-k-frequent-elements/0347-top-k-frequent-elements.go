5aeae69ca4f28257c762cd6d1dc2939244824a8c
Malik Fajar <43500332+malikfajr@users.noreply.github.com>
2023-09-15 03:12:53 +0700

    Time: 27 ms (7.31%), Space: 5.3 MB (86.47%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.go
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.go
@@ -1,24 +1,30 @@
-func topKFrequent(nums []int, k int) []int {
-    var counts = make(map[int]int)
-    
+func topKFrequent(nums[] int, k int)[] int {
+    var counts = make(map[int] int)
+
+    // cari angka unik beserta frekuensinya
     for i := 0; i < len(nums); i++ {
         counts[nums[i]] += 1;
     }
-    
-    var result = []int{}
-	for k > 0 {
-		i := -1
-		var v int
-		for num, count := range counts {
-			if i < count {
-			i = count
-			v = num
-			}
-		
-		}
-		result = append(result, v)
-		delete(counts, v)
-		k--;
-	}
-	return result
+
+    var result = [] int {}
+    for k > 0 {
+        i := -1 // frekunsi pasti diatas 0
+        var v int
+        
+        // cari nilai tertinggi
+        for num, count := range counts {
+            if i < count {
+                i = count
+                v = num
+            }
+
+        }
+        
+        // tampung dalam variabel,
+        // kemudian hapus untuk menghindari redundansi
+        result = append(result, v)
+        delete(counts, v)
+        k--;
+    }
+    return result
 }
