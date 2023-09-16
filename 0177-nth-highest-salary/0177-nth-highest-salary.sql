f375533039fc077baeefe11b179cc79750643136
Malik Fajar <43500332+malikfajr@users.noreply.github.com>
2023-09-16 19:10:31 +0700

    Time: 573 ms (99.68%), Space: 0B (100.00%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -4,8 +4,7 @@ BEGIN
   SET LINE = N - 1 ;
   RETURN (
       # Write your MySQL query statement below.      
-      SELECT  salary FROM Employee 
-      GROUP BY salary
+      SELECT  DISTINCT(salary) FROM Employee 
       ORDER BY salary DESC
       LIMIT 1 
       OFFSET LINE
