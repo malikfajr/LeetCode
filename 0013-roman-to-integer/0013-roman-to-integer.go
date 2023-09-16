9955ca56d2ee211f88e7f8c717ac0ab910c313cf
Malik Fajar <43500332+malikfajr@users.noreply.github.com>
2023-09-17 03:27:43 +0700

    Time: 0 ms (100.00%), Space: 2.7 MB (77.14%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.go
+++ 0013-roman-to-integer/0013-roman-to-integer.go
@@ -1,5 +1,5 @@
 func romanToInt(s string) int {
-    var keys = map[byte]uint16{
+    var keys = map[byte]int{
         'I': 1,
         'V': 5,
         'X': 10,
@@ -8,16 +8,18 @@ func romanToInt(s string) int {
         'D': 500,
         'M': 1000,
     }
-    var ans uint16 = 0
+    var ans = 0
+    var prev = 0;
     
-    for i := 0; i < len(s) - 1; i++ {
-        if keys[s[i]] < keys[s[i + 1]] {
-            ans = ans - keys[s[i]]
+    for i := len(s) - 1; i >= 0; i-- {
+        num := keys[s[i]]    
+        if num < prev {
+            ans = ans - num
         } else {
-            ans = ans + keys[s[i]]
+            ans = ans + num
         }
+        prev = num
     }
     
-    ans = ans + keys[s[len(s) - 1]]
-    return int(ans)
+    return (ans)
 }
